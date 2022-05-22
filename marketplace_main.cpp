b7268de31d26ce0af60d8542fbc82dfdbe8c1840
hannypm <101863648+hannypm@users.noreply.github.com>
2022-05-22 10:09:21 +0700

    Add files via upload
    Pascalino V Belling Bahy Putra - 2100018495
    Hanny Putri Maulana - 2100018496
    Athiyyah Alvita Putri - 2100018505

---
+++ marketplace_main.cpp
@@ -0,0 +1,12 @@
+#include "marketplace_class.h"
+#include "marketplace_input.h"
+#include "marketplace_appsAssets.h"
+#include "marketplace_proses.h"
+#include "marketplace_output.h"
+using namespace std;
+main()
+{
+    m.input();  // PASCAL
+    m.proses(); // TIYA
+    m.output(); // HANNY
+}
